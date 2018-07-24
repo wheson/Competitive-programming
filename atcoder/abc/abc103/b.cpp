501ecf9b7ffb1dc5f8eba6f1cc3137536a96e9b5
wheson <wheson.hxs@gmail.com>
2018-07-24 12:36:56 +0900

    ABC103に参加

---
+++ atcoder/abc/abc103/b.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+//#define int long long
+
+using namespace std;
+using LL = long long;
+using P = pair<int, int>;
+
+#define FOR(i, a, n) for(int i = (int)(a); i < (int)(n); ++i)
+#define REP(i, n) FOR(i, 0, n)
+
+#define pb(a) push_back(a)
+#define all(x) (x).begin(),(x).end()
+
+const int INF = (int)1e9;
+const LL INFL = (LL)1e18;
+const int MOD = 1e9 + 7;
+
+signed main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string s, t;
+    cin >> s >> t;
+
+    REP(i, s.size()){
+        if(s == t){
+            cout << "Yes" << endl;
+            return 0;
+        }
+        string ss = s.substr(0, s.size()-1);
+        s = s[s.size()-1] + ss;
+        cout << s << endl;
+    }
+    cout << "No" << endl;
+}
