56561c686bbf8768b200e84a2d821c2bc2583197
wheson <wheson.hxs@gmail.com>
2019-03-14 00:43:58 +0900

    AGC500点埋め

--- atcoder/agc/agc023/b.cpp
+++ atcoder/agc/agc023/b.cpp
@@ -4,34 +4,86 @@
 using namespace std;
 using LL = long long;
 using P = pair<int, int>;
-using Tapris = tuple<int, int, int>;
 
-#define REP(i, n) for(LL i = 0; i < n; ++i)
-#define FOR(i, a, n) for(LL i = a; i < n; ++i)
+#define FOR(i, a, n) for(int i = (int)(a); i < (int)(n); ++i)
+#define REP(i, n) FOR(i, 0, n)
+
 #define pb(a) push_back(a)
 #define all(x) (x).begin(),(x).end()
 
 const int INF = (int)1e9;
-const LL INFL = (LL)1e15;
+const LL INFL = (LL)1e19;
 const int MOD = 1e9 + 7;
 
-int dy[]={0, 0, 1, -1, 0};
-int dx[]={1, -1, 0, 0, 0};
-
-/*************** using variables ***************/
-int n;
-string s[305];
-/**********************************************/
+// Rabin-Karp string search algorithm
+class RollingHash{
+    private:
+      using ULL = unsigned long long;
+      int n;
+      const ULL mod = 1ULL << 63;
+      const ULL base = 1000000007ULL;
+      vector<ULL> hash, power;
+    public:
+      RollingHash(string s){
+          n = s.size();
+          hash.assign(n+1, 0ULL);
+          power.assign(n+1, 0ULL);
+          power[0] = 1ULL; 
+          for(int i = 0; i < n; i++){
+              power[i+1] = power[i] * base % mod;
+              hash[i+1] = (s[i] + hash[i] * base) % mod;
+          }
+      }
+      ULL GetHash(int l, int r){
+          return ((hash[r] - hash[l] * power[r-l]) % mod + mod) % mod;
+      }
+};
 
 signed main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-
-    cin >> n;
-    REP(i, n) cin >> s[i];
     
-    int ans = 0;
-    REP(i, n){
-        
+    int n;
+    cin >> n;
+    vector<string> s(n);
+    REP(i, n) cin >> s[i];
+
+    vector<string> h(n, ""), w = s;
+    REP(j, n){
+        REP(i, n){
+            h[j] = h[j] + s[i][j];
+        }
     }
+    REP(i, n){
+        h[i] = h[i] + h[i] + h[i];
+        w[i] = w[i] + w[i] + w[i];
+    }
+
+    vector<vector<bool>> check(n, vector<bool>(n, true));
+    vector<RollingHash> hhash, whash;
+
+    REP(i, n){
+        hhash.pb(RollingHash(h[i]));
+        whash.pb(RollingHash(w[i]));
+    }
+
+    REP(i, n){
+        REP(j, n){
+            REP(k, n-1){
+                int sz = n - k;
+                check[(i+n-k)%n][(j+n-k)%n] = check[(i+n-k)%n][(j+n-k)%n] 
+                    && hhash[j].GetHash(i+1, i+1+sz) == whash[i].GetHash(j+1, j+1+sz);
+                //cout << i << " " << j << " " << hhash[j].GetHash(i+1, i+1+sz) << " " << whash[i].GetHash(j+1, j+1+sz) << endl;
+                //cout << (i+n-k)%n << " " << (j+n-k)%n << " " << hh[j][(i+1)%n][sz] << " " << ww[i][(j+1)%n][sz] << endl;
+                //cout << (i+n-k)%n << " " << (j+n-k)%n << " " << h[j].substr(i+1, sz) << " " << w[i].substr(j+1, sz) << endl;
+                //cout << "h: " << j << " " << i+1 << " w: " << i << " " << j+1 << endl;
+            }
+        }
+    }
+    int ans = 0;
+    REP(i, n) REP(j, n){
+        //cout << i << " " << j << " " << (check[i][j] ? 1 : 0) << endl;
+        ans += check[i][j] ? 1 : 0;
+    }
+    cout << ans << endl;
 }
--- atcoder/agc/agc024/b.cpp
+++ atcoder/agc/agc024/b.cpp
@@ -12,23 +12,28 @@ using P = pair<int, int>;
 #define all(x) (x).begin(),(x).end()
 
 const int INF = (int)1e9;
-const LL INFL = (LL)1e15;
+const LL INFL = (LL)1e19;
 const int MOD = 1e9 + 7;
-
-int dy[]={0, 0, 1, -1, 0};
-int dx[]={1, -1, 0, 0, 0};
-
-/*************** using variables **************/
-int n;
-vector<int> p;
-/**********************************************/
 
 signed main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-
-    cin >> n;
-    p.resize(n);
-    REP(i, n) cin >> p[i];
     
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    REP(i, n) cin >> p[i];
+
+    vector<int> check(n+1, -1);
+
+    int sum = 0;
+    REP(i, n){
+        if(check[p[i] - 1] > 0){
+            check[p[i]] = check[p[i] - 1] + 1;
+        }else{
+            check[p[i]] = 1;
+        }
+        sum = max(check[p[i]], sum);
+    }
+    cout << n - sum << endl;
 }
